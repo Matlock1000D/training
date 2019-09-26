46bd00f1843375afa8baa7257d632ed0fa942ae0
Matlock1000D <urja-sdp@gmail.com>
2019-09-26 10:09:42 +0300

    Uusi projekti luokilla leikkimiseen.

---
+++ LuokkaPelleily/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LuokkaPelleily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
