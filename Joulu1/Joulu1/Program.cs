b949621e50f318c1494ac3b553be327c46b2ae45
Matlock1000D <urja-sdp@gmail.com>
2019-12-03 12:12:34 +0200

    Ekan päivän joulukalenteritehtävät”

--- Joulu1/Joulu1/Program.cs
+++ Joulu1/Joulu1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Joulu1
 {
@@ -6,7 +7,24 @@ namespace Joulu1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] lines = File.ReadAllLines(@"data\Joulu1.txt");
+            int totalfuel = 0;
+
+            foreach (string line in lines)
+            {
+                int mass = Int32.Parse(line);
+                int addfuel = mass;
+                int modfuel = 0;
+                do
+                {
+                    addfuel = addfuel / 3;
+                    addfuel -= 2;
+                    if (addfuel < 0) addfuel = 0;
+                    modfuel += addfuel;
+                } while (addfuel > 0);
+                totalfuel += modfuel;
+            }
+            Console.WriteLine(totalfuel);
         }
     }
 }
