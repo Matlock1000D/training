c56b67b2f08cbff4c70da5f80841330ded01afa5
Matlock1000D <urja-sdp@gmail.com>
2019-12-04 14:32:33 +0200

    Kakkospäivän eka tehtävä rekonstruoitu.

--- Joulu1/Joulu1/Program.cs
+++ Joulu1/Joulu1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 namespace Joulu1
 {
@@ -17,10 +18,47 @@ namespace Joulu1
                 case "1":
                     Day1();
                     break;
+                case "2":
+                    Day2();
+                    break;
                 default:
-                    Console.WriteLine("Anna kunnon päivä");
+                    Console.WriteLine("Anna kunnon päivä!");
                     break;
             }
+        }
+
+        static void Day2()
+        {
+            string inputdata = File.ReadAllText(@"data\Joulu2.txt");
+            string[] intopStrings = inputdata.Split(',');
+            List<int> intopsList = new List<int>();
+            int commandpointer = 0;
+
+            foreach(string intopString in intopStrings)
+            {
+                intopsList.Add(Int32.Parse(intopString));
+            }
+            int[] intops = intopsList.ToArray();
+
+            while(intops[commandpointer] != 99)
+            {
+                switch (intops[commandpointer])
+                {
+                    case 1:
+                        intops[intops[commandpointer + 3]] = intops[intops[commandpointer + 1]] + intops[intops[commandpointer + 2]];
+                        break;
+                    case 2:
+                        intops[intops[commandpointer + 3]] = intops[intops[commandpointer + 1]] * intops[intops[commandpointer + 2]];
+                        break;
+                    default:
+                        Console.WriteLine("Määrittelemättömiä käskyjä syötteessä!");
+                        return;
+                        break;
+                }
+
+                commandpointer += 4;
+            }
+            Console.WriteLine(intops[0]);
         }
 
         static void Day1()
