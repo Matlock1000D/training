1323e191c735db727211035cafb004a31c7870fd
Matlock1000D <urja-sdp@gmail.com>
2020-01-09 09:29:39 +0200

    Pyytton-kikkailua

--- Pyyttoni1/Pyyttoni1/Pyyttoni1.py
+++ Pyyttoni1/Pyyttoni1/Pyyttoni1.py
@@ -1,11 +1,14 @@
 
 #Testikoodia pyyttonilla kikkailuun
 
-thisdict = {
-    "merkki": "Lada",
-    "malli": "1200S",
-    "vuosi": 1985
-    }
+class ParasLuokka:
+    def __init__(self, nimi, ikä):
+        self.nimi = nimi
+        self.ikä = ikä
 
-for x,y in thisdict.items():
-    print(x,y)
+    def declare(self):
+        print("Hei, olen " + self.nimi)
+
+p1 = ParasLuokka("Juhani",36)
+
+p1.declare()
